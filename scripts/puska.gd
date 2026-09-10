4197fc5b0817acfe48d117c36c2a427be93ae82b
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-09-10 15:58:43 +0200

    muzzle flash

--- scripts/puska.gd
+++ scripts/puska.gd
@@ -2,12 +2,12 @@ extends Node3D
 
 
 @onready var animation_player: AnimationPlayer = $puska/AnimationPlayer
+@onready var muzzle: GPUParticles3D = $muzzleFlash/muzzle
 
 @onready var reloading = false
 
 
 func reload() -> void:
-	reloading = true
 	animation_player.play("reload")
 	await animation_player.animation_finished
 	reloading = false
@@ -15,5 +15,7 @@ func reload() -> void:
 
 func shoot() -> void:
 	animation_player.play("shoot")
+	muzzle.emitting = true
+	reloading = true
 	await animation_player.animation_finished
 	reload()
