b9e824d2dca46af908870d41ffe4a8598b29667e
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-09-10 20:28:28 +0200

    eksplozija event fix

--- scripts/galeb_eksplozija.gd
+++ scripts/galeb_eksplozija.gd
@@ -2,6 +2,9 @@ class_name GalebEksplozija
 extends Node3D
 
 
+signal finished
+
+
 @onready var krv: GPUParticles3D = $krv
 @onready var perje: GPUParticles3D = $perje
 @onready var perje_2: GPUParticles3D = $perje2
@@ -11,3 +14,15 @@ func explode() -> void:
 	krv.emitting = true
 	perje.emitting = true
 	perje_2.emitting = true
+
+
+func _on_krv_finished() -> void:
+	finished.emit()
+
+
+func _on_perje_finished() -> void:
+	finished.emit()
+
+
+func _on_perje_2_finished() -> void:
+	finished.emit()
