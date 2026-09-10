d119cd64896021b21118b580fc0ed064f0d8d3f1
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-09-10 18:24:33 +0200

    eksplozija sada ima i braon perje

--- scripts/galeb_eksplozija.gd
+++ scripts/galeb_eksplozija.gd
@@ -4,10 +4,10 @@ extends Node3D
 
 @onready var krv: GPUParticles3D = $krv
 @onready var perje: GPUParticles3D = $perje
+@onready var perje_2: GPUParticles3D = $perje2
 
 
 func explode() -> void:
-	krv.emitting = false
 	krv.emitting = true
-	perje.emitting = false
 	perje.emitting = true
+	perje_2.emitting = true
